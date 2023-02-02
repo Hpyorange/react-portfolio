35eac7a24deb9f1a502cd17aa622d1133bdeda79
Hpyorange <jaspercheng2022@gmail.com>
2023-02-02 14:09:28 -0500

    complete contact me page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,20 +4,34 @@ import '../styles/main.css'
 export default function Contact() {
   return (
     <div class="content">
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1> <span className='contactSpan'>Contact</span> Me</h1>
+
+    <div class="d-flex flex-column justify-content-center">
+      <div class="mb-3 row">
+
+      <label for="exampleFormControlInput1" class="form-label">Name</label>
+      <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Please Enter Your Name"/>
+
+      </div>
+
+      <div class="mb-3 row">
+
+        <label for="exampleFormControlInput2" class="form-label">Email</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+
+      </div>
+
+      <div class="mb-3 row">
+        <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"  placeholder="Your Message"></textarea>
+      </div>
+
+      <div class="d-flex align-items-center justify-content-center text-center">
+        <button class="mybtn text-decoration-none">Submit</button>
+      </div>
+
+    </div>
+
     </div>
   );
 }
