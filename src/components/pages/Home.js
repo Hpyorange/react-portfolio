b072b8e756e1c27e7c049872ee6e48d37d3564c6
Hpyorange <jaspercheng2022@gmail.com>
2023-02-01 23:55:45 -0500

    swiper update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../styles/main.css';
-import mySwiper from './swiper';
+import Sw from './swiper';
 
 export default function Home() {
   return (
     <div class="content">
-      <h1 class="title">Hi, My name is <span>Jasper</span></h1>
-      <h4 class='spanColor'>Welcome to my portfolio site</h4>
-      <mySwiper/>
+      <div className='mb-5 text-center'>
+        <h1 class="title">Hi, My name is <span>Jasper</span></h1>
+        <h4 class='spanColor'>Welcome to my portfolio site</h4>
+      </div>
+      <Sw />
     </div>
   );
 }
--- src/components/pages/swiper.jsx
+++ src/components/pages/swiper.jsx
@@ -1,33 +1,39 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import React from "react";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import "../styles/main.css";
 
-// import required modules
-import { Pagination } from "swiper";
+import { Navigation, Pagination} from "swiper";
 
-export default function mySwiper() {
+
+
+export default function sw1() {
   return (
+    <>
       <Swiper
-        slidesPerView={3}
+        slidesPerView={4}
         spaceBetween={30}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
+        navigation={true}
+        modules={[Pagination,Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide><div class="myCard">Slide 1</div></SwiperSlide>
+        <SwiperSlide><div class="myCard">Slide 2</div></SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
         <SwiperSlide>Slide 6</SwiperSlide>
+
       </Swiper>
+    </>
   );
 }
