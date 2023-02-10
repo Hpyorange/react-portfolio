535264a1fa30a51481808d5c63c2f5905d56b959
Hpyorange <jaspercheng2022@gmail.com>
2023-02-10 18:52:07 -0500

    change about me info

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
         <img src={photo} alt="Jasper" />
         <p class="container mx-4 aboutText col-12 col-md-6">
       UI/UX Designer & Front End Developer
-      with B.S. in computer science and a certificate from UofT SCS UX/UI Boot Camp. Skilled in most Adobe suite software, Figma, HTML5, CSS, JavaScript, Bootstrap, and jQuery. Strengthened in user problem solving and believed that understanding the needs and frustrations is the key to improving user experience. Specializing in creating beautiful and functional websites and digital interfaces, I am a very passionate designer who always wants to grow, learn, and explore something new.
+      with B.S. in computer science and certificates from UofT SCS UX/UI and full stack web development Boot Camp. Skilled in most Adobe suite software, Figma, HTML5, CSS, JavaScript, TypeScript, Bootstrap, jQuery and React. Strengthened in user problem-solving and believed that understanding the needs and frustrations is the key to improving user experience. Specializing in creating and coding beautiful and functional websites and digital interfaces, I am a very passionate designer/developer who always wants to grow, learn, and explore something new.
       </p>
       </div>
 
